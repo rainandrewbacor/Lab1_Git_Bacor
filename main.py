e38d7d5659e1772cf3456e490378d9adb747a2d1
Rain Andrew F. Bacor <rainandrew.bacor@tup.edu.ph>
2026-09-09 15:40:11 +0800

    Added section info for Bacor.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Lab 1: Version Control")
 print("Author: RAIN ANDREW F. BACOR") # <--- REPLACE WITH YOUR NAME
 print("Student ID: TUPM-26-1879") # <--- REPLACE WITH YOUR ID
+print("Class Section: BSECE-1A")
